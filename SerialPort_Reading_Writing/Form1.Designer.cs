f649d22f0cba53aa02fe00966afca49ea737bed0
Amit Tomar <theamittomar@gmail.com>
2021-01-11 11:22:09 +0530

    Add project files.

---
+++ SerialPort_Reading_Writing/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace SerialPort_Reading_Writing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtReceiveData = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rtSendData = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtReceiveData
+            // 
+            this.rtReceiveData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtReceiveData.Location = new System.Drawing.Point(12, 33);
+            this.rtReceiveData.Name = "rtReceiveData";
+            this.rtReceiveData.Size = new System.Drawing.Size(776, 190);
+            this.rtReceiveData.TabIndex = 0;
+            this.rtReceiveData.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Received Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Type to send data";
+            // 
+            // rtSendData
+            // 
+            this.rtSendData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtSendData.Location = new System.Drawing.Point(2, 280);
+            this.rtSendData.Name = "rtSendData";
+            this.rtSendData.Size = new System.Drawing.Size(786, 101);
+            this.rtSendData.TabIndex = 2;
+            this.rtSendData.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(635, 387);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(153, 51);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtSendData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtReceiveData);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtReceiveData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox rtSendData;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ SerialPort_Reading_Writing/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialPort_Reading_Writing
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPortData;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                /*
+                 * Serial Port Integration Classes and Property
+                 * Serial Port will be opend on CheckBox click and even on Form Closing It will be closed
+                 */
+                serialPortData = new SerialPort();
+
+                serialPortData.PortName = ConfigurationManager.AppSettings["ComPort"].ToString();
+                serialPortData.BaudRate = int.Parse(ConfigurationManager.AppSettings["BaudRate"].ToString());
+                serialPortData.Parity = (Parity)Enum.Parse(typeof(Parity), ConfigurationManager.AppSettings["Parity"].ToString());
+                serialPortData.StopBits = (StopBits)Enum.Parse(typeof(StopBits), ConfigurationManager.AppSettings["StopBits"].ToString());
+                serialPortData.DataBits = int.Parse(ConfigurationManager.AppSettings["DataBits"].ToString());
+                serialPortData.Handshake = Handshake.None;
+                serialPortData.ReadTimeout = 2000;
+                serialPortData.DataReceived += SerialPortData_DataReceived;
+                OpenSerialPort();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        #region Serial Port Communication
+
+        private bool OpenSerialPort()
+        {
+            try
+            {
+                if (serialPortData.IsOpen) return true;
+                else
+                {
+                    serialPortData.Open(); return true;
+                }
+            }
+            catch (Exception ex) { throw ex; }
+        }
+        private void CloseSerialPort()
+        {
+            try
+            {
+                if (serialPortData != null && serialPortData.IsOpen)
+                {
+                    serialPortData.Close();
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        string readData = "";
+        private void SerialPortData_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                SerialPort serialPort = sender as SerialPort;
+                if (serialPortData.IsOpen)
+                {
+                    string receivedData = serialPort.ReadLine();
+                    this.Invoke(new Action(() =>
+                    {
+                        try
+                        {
+                            rtReceiveData.Text = receivedData;
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Invoke(new Action(() => MessageBox.Show(ex.Message)));
+            }
+        }
+
+        #endregion
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                /*
+                * Weighing Machine Integration
+                */
+                if (serialPortData != null && serialPortData.IsOpen)
+                {
+                    serialPortData.Close();
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPortData.IsOpen)
+                {
+                    serialPortData.WriteLine(rtSendData.Text);
+                }
+                else
+                    MessageBox.Show("Serial port is not opened");
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+    }
+}
